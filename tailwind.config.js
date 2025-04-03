e4ba3b4734ad033b8fbae6c38e78a2b4fb9873d4
jackiesparrow11 <157010255+jackiesparrow11@users.noreply.github.com>
2025-04-03 09:56:32 +0800

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
+/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
